96497e1002fd970f511f5d4380582a7ff6cc2976
Adam Morris <aimorris@aimorris.com>
2021-02-23 19:42:53 +1000

    no mapped volume

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,7 +1,7 @@
 battery=$(acpi | grep -o '[0-9]*%')
 date=$(date "+%A %d/%m/%y %T")
-volume=$(amixer -M get Master | grep -o '[0-9]*%')
-muted=$(amixer -M get Master | tail -2 | grep -c '\[on\]')
+volume=$(amixer get Master | grep -o '[0-9]*%')
+muted=$(amixer get Master | tail -2 | grep -c '\[on\]')
 
 if [[ $muted -ne 1 ]]
 then
