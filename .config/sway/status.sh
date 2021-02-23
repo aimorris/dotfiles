ba2e41d4335572ae3816a6c1d72422fd20677c2e
Adam Morris <aimorris@aimorris.com>
2021-02-23 19:27:16 +1000

    muted status

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,5 +1,11 @@
 battery=$(acpi | grep -o '[0-9]*%')
 date=$(date "+%A %d/%m/%y %T")
 volume=$(amixer -M get Master | grep -o '[0-9]*%')
+muted=$(amixer -M get Master | tail -2 | grep -c '\[on\]')
 
-echo "$volume | $battery | $date"
+if [[ $muted -ne 1 ]]
+then
+	echo "MUTED | $battery | $date"
+else
+	echo "$volume | $battery | $date"
+fi
