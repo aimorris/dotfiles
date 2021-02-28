09439ebc9d8c892c992f4b1b16d5acdb9a4a3e9d
Adam Morris <aimorris@aimorris.com>
2021-02-28 15:01:00 +1000

    move to pulse

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -1,7 +1,7 @@
 battery=$(acpi | grep -o '[0-9]*%')
 date=$(date "+%A %d/%m/%y %T")
-volume=$(amixer get Master | grep -o '[0-9]*%')
-muted=$(amixer get Master | tail -2 | grep -c '\[on\]')
+volume=$(amixer -D pulse get Master | grep -o '[0-9]*%' -m 1)
+muted=$(amixer -D pulse get Master | tail -1 | grep -c '\[on\]')
 
 if [[ $muted -ne 1 ]]
 then
