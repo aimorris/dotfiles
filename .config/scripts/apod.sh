30f7b9d6b7bed614bf2a00f7a6d849cfdc1f5ff0
Adam Morris <aimorris@aimorris.com>
2021-03-05 14:25:05 +1000

    apod

---
+++ .config/scripts/apod.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Image in ~/.apodwallpaper 
+FILENAME=apodwallpaper
+APODWALLPAPER=${HOME}/.${FILENAME}
+
+rm -rf  ${APODWALLPAPER}
+mkdir -p ${APODWALLPAPER} && cd ${APODWALLPAPER}
+
+# download image from apod site
+wget -A.jpg -r -l1 --no-parent -nH https://apod.nasa.gov/apod/astropix.html
+
+# move image from obscure folder to main folder, rename image
+find ./apod -name "*.gif" -or -name "*.jpg" | while read line ; do
+mv "$line" "${FILENAME}.jpg"
+done
+
+# Set as wallpaper
+feh --bg-scale "${APODWALLPAPER}/${FILENAME}.jpg"
+
+#get rid of cruft
+rm -rf apod robots.txt robots.txt.tmp
